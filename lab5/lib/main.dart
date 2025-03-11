edc8c90681314cd4c03985f9f170fac64e36ef6d
Alaska16 <sokol021004@gmail.com>
2025-03-11 16:24:57 +0300

    add:lab5.1

---
+++ lab5/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Список элементов',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Список элементов'),
+        ),
+        body: ListView(
+          children: const [
+            Text('0000'),
+            Divider(),
+            Text('0001'),
+            Divider(),
+            Text('0010'),
+          ],
+        ),
+      ),
+    );
+  }
+}
