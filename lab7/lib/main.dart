b8ae955e1b2f6230db0ab98e09afe6df4b833eb7
Alaska16 <sokol021004@gmail.com>
2025-03-21 11:11:31 +0300

    final: lab7 (style)

--- lab7/lib/main.dart
+++ lab7/lib/main.dart
@@ -6,14 +6,24 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Возвращение значения')),
-      body: Center(child: ElevatedButton(
+      appBar: AppBar(
+          title: const Text('Возвращение значения'),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+      ),
+      body: Center(child:
+      ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
+        ),
         onPressed: () {
           Navigator.push(context, MaterialPageRoute(
             builder: (context) => const SecondScreen(),
           ));
         },
-        child: const Text('Приступить к выбору'),
+        child: const Text('Приступить к выбору',
+            style: TextStyle(color: Colors.white),
+        ),
       )),
     );
   }
@@ -25,27 +35,41 @@ class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Выберите любой вариант')),
+      appBar: AppBar(
+          title: const Text('Выберите любой вариант'),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+      ),
       body: Center(child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
+            ),
             onPressed: () {
               Navigator.pop(context);
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(content: Text('Да!')),
               );
             },
-            child: const Text('Да!'),
+            child: const Text('Да!',
+                style: TextStyle(color: Colors.white),
+            ),
           ),
           ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
+            ),
             onPressed: () {
               Navigator.pop(context);
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(content: Text('Нет.')),
               );
             },
-            child: const Text('Нет.'),
+            child: const Text('Нет.',
+                style: TextStyle(color: Colors.white),
+            ),
           ),
         ],
       )),
