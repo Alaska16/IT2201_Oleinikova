3db43f10a4ea1d799782fe0e9870ea6a4af52bf7
Alaska16 <sokol021004@gmail.com>
2025-04-08 01:29:21 +0300

    lab8.1: create classes

--- lab8_1/lib/main.dart
+++ lab8_1/lib/main.dart
@@ -7,7 +7,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Фотогалерея',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -20,7 +27,19 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class Photo {
+  final String id;
+  final String url;
+  final String thumbUrl;
 
+  Photo({required this.id, required this.url, required this.thumbUrl});
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      id: json['id'],
+      url: json['urls']['regular'],
+      thumbUrl: json['urls']['thumb'],
+    );
+  }
 }
 
 class PhotoList extends StatelessWidget {
