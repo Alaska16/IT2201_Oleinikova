a1583a3bbc529672e47e90b627afbc6f555c6680
Alaska16 <sokol021004@gmail.com>
2025-04-08 01:22:26 +0300

    lab8.1: create classes

---
+++ lab8_1/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+}
+
+class Photo {
+
+}
+
+class PhotoList extends StatelessWidget {
+
+}
