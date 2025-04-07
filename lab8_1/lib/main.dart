0cfec62c32af8e8d9acf45260d63c5d10957f0c2
Alaska16 <sokol021004@gmail.com>
2025-04-08 01:45:01 +0300

    lab8.1: edit 2

--- lab8_1/lib/main.dart
+++ lab8_1/lib/main.dart
@@ -24,7 +24,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<Photo>> futurePhotos;
 
+  @override
+  void initState() {
+    super.initState();
+    futurePhotos = fetchPhotos();
+  }
+
+  Future<List<Photo>> fetchPhotos() async {
+    final response = await http.get(
+      Uri.parse(
+          ''
+      ),
+    );
+    if (response.statusCode == 200) {
+      return parsePhotos(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load photos');
+    }
+  }
+
+  List<Photo> parsePhotos(List<dynamic> json) {
+    return json.map((photo) => Photo.fromJson(photo)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Фотогалерея')),
+      body: FutureBuilder<List<Photo>>(
+        future: futurePhotos,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Ошибка: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('Нет данных'));
+          } else {
+            return PhotoList(photos: snapshot.data!);
+          }
+        },
+      ),
+    );
+  }
 }
 
 class Photo {
